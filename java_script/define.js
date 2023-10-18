3889b8dd9120205aeb5cc01668af931f53f67b6b
toai-nguyen <maxngudan@gmail.com>
2023-10-18 22:43:32 +0700

    update responsive bar mobi

--- java_script/define.js
+++ java_script/define.js
@@ -2,3 +2,9 @@ let header = document.getElementById("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", this.window.scrollY > 0)
 })
+
+let addBtn = document.getElementById("add");
+addBtn.addEventListener("click", function(){
+    addBtn.classList.toggle("active");
+    header.classList.toggle("show");
+})
